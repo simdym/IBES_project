961f4f671e23bce2078e2bed9f306ac60fc5d70d
Simen Dymbe <simen.dymbe@gmail.com>
2022-10-17 19:16:12 +0200

    Add working example from GItHub

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from bitalino import BITalino
 
-macAddress = "00:00:00:00:00:00"
+macAddress = "98:D3:31:B1:84:2C"
 
-bitalino = BITalino(macAddress)
+batteryThreshold = 30
+acqChannels = [0, 3]
+samplingRate = 1000
+nSamples = 10
+digitalOutput = [0, 0, 1, 1]
 
+# Connect to BITalino
+device = BITalino(macAddress)
+
+# Set battery threshold
+device.battery(batteryThreshold)
+
+# Read BITalino version
+print(device.version())
+
+# Start Acquisition
+device.start(samplingRate, acqChannels)
+
+# Read samples
+print(device.read(nSamples))
+
+# Turn BITalino led on
+device.trigger(digitalOutput)
+
+# Stop acquisition
+device.stop()
+
+# Close connection
+device.close()
