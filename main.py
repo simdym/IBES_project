958fdf7ac1ebf6ad1d552f3e8936d5443baebf2f
Simen Dymbe <simen.dymbe@gmail.com>
2022-12-04 15:55:13 +0100

    Add plotting of peaks

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from bitalino import BITalino
 import live_plotter
 
-macAddress = "98:D3:71:FD:61:F4"
+macAddress = "98:D3:41:FD:4E:E5"
 
 batteryThreshold = 30
-acqChannels = [0, 3]
+acqChannels = [1,2]
 samplingRate = 1000
-nSamples = 10
+nSamples = 100
 digitalOutput = [0, 0, 1, 1]
 
 # Connect to BITalino
@@ -22,7 +22,9 @@ print("Version", device.version())
 # Start Acquisition
 device.start(samplingRate, acqChannels)
 
-live_plotter.live_plotter(device, [4,5,6], 10)
+#device = live_plotter.mock_dev()
+
+live_plotter.live_plotter(device, [5], nSamples, display_len=20000, window_len=20000, plot_type='peaks')
 
 # Read samples
 samples = device.read(nSamples)
@@ -30,6 +32,7 @@ print(samples.shape)
 
 # Turn BITalino led on
 #device.trigger()
+
 
 # Stop acquisition
 device.stop()
